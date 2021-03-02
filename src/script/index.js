fdc5b8948955eaaa9c9653ed910c8d3514c3ee9c
Roland Baldovino <ro@curiouslab.sg>
2021-03-02 15:42:58 +0800

    Separate experience for parallax and normal model view

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { USDZExporter } from 'three/examples/jsm/exporters/USDZExporter.js'
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { DeviceOrientationControls } from 'three/examples/jsm/controls/DeviceOrientationControls.js'
 
 import fragment from '../assets/shaders/fragment.glsl'
 import vertex from '../assets/shaders/vertex.glsl'
@@ -12,50 +13,116 @@ import gsap from 'gsap'
 
 export default class AdPortal {
   constructor(options) {
-		this.options = options
+		const t = this
+		t.options = options
+		t.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+		t.isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) && !window.MSStream
+		t.isPlaying = false
+		t.isParallax = false
 
-    this.scene = new THREE.Scene()
-		this.scene.fog = new THREE.Fog(options.bgColor, options.near, 88)
-    this.scene.background = new THREE.Color(options.bgColor)
+		if(t.isMobile) {
+			const startButton = document.getElementById('start')
+			startButton.classList.remove('hidden')
+			startButton.addEventListener( 'click', function () {
+				startButton.remove()
 
-    this.container = options.dom
-    this.width = this.container.offsetWidth
-    this.height = this.container.offsetHeight
-    this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    this.renderer.setSize(this.width, this.height)
-    this.renderer.setClearColor(options.bgColor, 1)
+				t.isPlaying = false
+				t.renderer.clear()
+				t.renderer.dispose()
+				t.container.innerHTML = ''
 
-		this.renderer.toneMapping = THREE.ACESFilmicToneMapping
-		this.renderer.toneMappingExposure = 1
-		this.renderer.outputEncoding = THREE.sRGBEncoding
+				t.isParallax = true
 
-    this.container.appendChild(this.renderer.domElement)
+				t.init()
+			} )
+		}
 
-    this.camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
-      1000
-    )
-
-    this.camera.position.set(0, 0, 3)
-
-		this.environment = new RoomEnvironment()
-		this.pmremGenerator = new THREE.PMREMGenerator( this.renderer )
-		this.scene.environment = this.pmremGenerator.fromScene( this.environment ).texture
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.time = 0
-
-    this.isPlaying = true
-
-    this.addObjects()
-
-    window.addEventListener("resize", this.resize.bind(this))
-    this.resize()
-    // this.settings()
+		t.debug =  document.getElementById('debug')
+		this.loadModel()
   }
+
+	init() {
+		const t = this
+		t.scene = new THREE.Scene()
+		// this.scene.fog = new THREE.Fog(this.options.bgColor, this.options.near, 88)
+    // this.scene.background = new THREE.Color(this.options.bgColor)
+
+    t.container = t.options.dom
+    t.width = t.container.offsetWidth
+    t.height = t.container.offsetHeight
+    t.renderer = new THREE.WebGLRenderer()
+    t.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    t.renderer.setSize(t.width, t.height)
+    t.renderer.setClearColor(t.options.bgColor, 1)
+
+		t.renderer.toneMapping = THREE.ACESFilmicToneMapping
+		t.renderer.toneMappingExposure = 1
+		t.renderer.outputEncoding = THREE.sRGBEncoding
+
+    t.container.appendChild(t.renderer.domElement)
+
+    t.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, t.options.near, 100 )
+
+    t.camera.position.set(0, 0, 3)
+		t.camera.lookAt(0,0,0)
+
+		t.oControls = new OrbitControls(t.camera, t.renderer.domElement)
+
+		t.debug.innerHTML = 'init'
+
+		if(t.isMobile && t.isParallax) {
+			t.rotation = new DeviceOrientationControls(new THREE.PerspectiveCamera())
+		}
+
+		t.environment = new RoomEnvironment()
+		t.pmremGenerator = new THREE.PMREMGenerator( t.renderer )
+		t.scene.environment = t.pmremGenerator.fromScene( t.environment ).texture
+
+    t.time = 0
+    t.isPlaying = true
+
+    t.scene.add(t.gltf.scene)
+		t.render()
+
+    window.addEventListener("resize", t.resize.bind(t))
+    t.resize()
+
+		if(t.isIOS) document.getElementById('usdzCta').classList.remove('hidden')
+
+    // this.settings()
+	}
+
+	loadModel() {
+		const t = this
+
+		const loader = new GLTFLoader().setPath('models/gltf/')
+		loader.load('MaterialsVariantsShoe.gltf',
+			async (gltf) => {
+				const scale = 8
+				gltf.scene.scale.set(scale, scale, scale)
+				t.gltf = gltf
+
+				t.exportUSDZ(gltf)
+
+				t.init()
+			},
+			(xhr) => {
+				console.log((xhr.loaded / xhr.total * 100 ) + '% loaded')
+			},
+			(error) => {
+				console.log(error)
+			}
+		)
+  }
+
+	async exportUSDZ (gltf) {
+		const exporter = new USDZExporter()
+		const arraybuffer = await exporter.parse( gltf.scene )
+		const blob = new Blob( [ arraybuffer ], { type: 'application/octet-stream' } )
+
+		const link = document.getElementById( 'usdz' )
+		link.href = URL.createObjectURL( blob )
+	}
 
   settings() {
     this.settings = {
@@ -72,41 +139,6 @@ export default class AdPortal {
     this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
   }
-
-  addObjects() {
-		const t = this
-
-		const loader = new GLTFLoader().setPath( 'models/gltf/' )
-		loader.load('MaterialsVariantsShoe.gltf',
-			async (gltf) => {
-				const scale = 8
-				gltf.scene.scale.set( scale, scale, scale )
-				t.gltf = gltf
-				t.scene.add(gltf.scene)
-				t.render()
-
-				t.exportUSDZ(gltf)
-			},
-			(xhr) => {
-				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-			},
-			(error) => {
-				console.log(error)
-			}
-		)
-
-		this.addStage()
-		this.addLights()
-  }
-
-	async exportUSDZ (gltf) {
-		const exporter = new USDZExporter()
-		const arraybuffer = await exporter.parse( gltf.scene )
-		const blob = new Blob( [ arraybuffer ], { type: 'application/octet-stream' } )
-
-		const link = document.getElementById( 'usdz' )
-		link.href = URL.createObjectURL( blob )
-	}
 
 	addStage() {
 
@@ -142,11 +174,32 @@ export default class AdPortal {
     }
   }
 
-  render() {
-    if (!this.isPlaying) return;
+	updateUserCamM() {
+		if (!this.rotation) return
 
-    requestAnimationFrame(this.render.bind(this));
-    this.renderer.render(this.scene, this.camera);
+    this.rotation.update()
+
+    if (!this.rotation.deviceOrientation) return
+
+    const { beta, gamma } = this.rotation.deviceOrientation
+		this.debug.innerHTML = `orientattion: ${beta}, ${gamma}, ${this.rotation}`
+
+    if (!beta || !gamma) return
+
+		this.camera.lookAt(0, 0, 0)
+
+    this.camera.position.x = -gamma / 90
+    this.camera.position.y = beta / 90
+    this.camera.position.z = 1 - 0.5 * Math.min(Math.abs(this.camera.position.x) + Math.abs(this.camera.position.y), 1)
+	}
+
+  render() {
+    if (!this.isPlaying) return
+
+		if(this.isMobile) this.updateUserCamM()
+
+    requestAnimationFrame(this.render.bind(this))
+    this.renderer.render(this.scene, this.camera)
   }
 }
 
